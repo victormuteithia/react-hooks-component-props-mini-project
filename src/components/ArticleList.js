7eaf9dd63f823ea06e05dcb9d4f07871df5ea02d
victormuteithia <muteithiavictor@gmail.com>
2024-02-03 20:15:40 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import Article from "./Article";
+
+function ArticleList({posts}){
+    return(
+        <main>
+            {posts.map((post) => {
+                return(
+                    <Article
+                    key={post.id}
+                    title={post.title}
+                    date={post.date}
+                    preview={post.preview}
+                    />
+                )
+            })}
+        </main>
+    )
+}
+
+export default ArticleList;
